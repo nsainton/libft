e0e3c5517989b144450cd8a2e6b7d50c973ae296
nsainton <nsainton@student.42.fr>
2023-07-28 09:43:42 +0200

    Modified function to del str to delete the string and the object

--- sources/tstr_del.c
+++ sources/tstr_del.c
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 05:25:38 by nsainton          #+#    #+#             */
-/*   Updated: 2023/07/28 09:23:50 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/07/28 09:43:16 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ void	tstr_del(t_str *str)
 {
 	if (str->size > 0)
 		free(str->str);
-	ft_bzero(str, sizeof * str);
+	free(str);
 }
